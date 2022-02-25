2336c4dfe4a6de1a90b37c3dbb977bd9e42f3b32
Legolas <geran.vanatta@gmail.com>
2022-02-24 23:56:14 -0600

    console log statements when an theres no answer input

--- Develop/index.js
+++ Develop/index.js
@@ -6,48 +6,113 @@ const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'Name',
-            message: 'What is your project title?'
+            name: 'title',
+            message: 'What is your project title?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a project title!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Description',
-            message: 'Provide a description for your project.'
+            message: 'Provide a description for your project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a project description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Table of Contents',
-            message: 'input a table of contents to make it easy for users to find what they need.'
+            message: 'input a table of contents to make it easy for users to find what they need.',
+            validate: contentInput => {
+                if (contentInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter the table of contents for your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Installation',
-            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter the steps to install your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Usage',
-            message: 'Provide instructions and examples for use.'
+            message: 'Provide instructions and examples for use.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter instructions for your project!');
+                    return false;
+                }
+            }
         },
         {
-            type: 'input',
+            type: 'checkbox',
             name: 'License',
-            message: 'Input the license for your project.'
+            message: 'Select the license you want to use for your project.',
+            choices: ['MPL', 'CPL', 'MIT', 'BSD', 'APACHE', 'Ms-RL']   
         },
         {
             type: 'input',
             name: 'Contributing',
-            message: 'Input your guidelines for how other developers can contribute your work.'
+            message: 'Input your guidelines for how other developers can contribute your work.',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter contribution guidelines!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Tests',
-            message: 'Provide examples for how to run tests for your project.'
+            message: 'Provide examples for how to run tests for your project.',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter test instructions for your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Questions',
-            message: 'Enter your contact info in case another developer has questions about your project.'
+            message: 'Enter your contact info in case another developer has questions about your project.',
+            validate: questionInput => {
+                if (questionInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter your contact info (e.g. your email)!');
+                    return false;
+                }
+            }
         },
     ]);
 };
